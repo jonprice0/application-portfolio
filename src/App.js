0d69bf1113e6dc479365c00ce79095c63c891f0a
Jonathan Price <jonprice0@gmail.com>
2022-08-03 10:09:31 -0400

    edited App.js to reflect reorganization of Header/Navigation components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Nav from './components/Nav';
+import Header from './components/Header';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div>
-      <Nav></Nav>
+      <Header></Header>
       <main>
         <About></About>
         <Portfolio></Portfolio>
